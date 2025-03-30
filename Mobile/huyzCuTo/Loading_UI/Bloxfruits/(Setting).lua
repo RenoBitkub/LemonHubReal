12e798205697023d21cc93b596b7c22e108480d3
RenoBitkub <LemonBitKubCommunity@gmail.com>
2025-03-30 19:57:20 +0700

    Mobile/huyzCuTo/Loading_UI/Bloxfruits/( Setting ).lua

--- Mobile/huyzCuTo/Loading_UI/Bloxfruits/(Setting).lua
+++ Mobile/huyzCuTo/Loading_UI/Bloxfruits/(Setting).lua
@@ -42,6 +42,6 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
   Translator = true; -- true/false
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RenoBitkub/LemonHub/refs/heads/main/Mobile/Limited/huyzkuto/Loading_UI/MainBloxFruit.lua")()
   end
   })
