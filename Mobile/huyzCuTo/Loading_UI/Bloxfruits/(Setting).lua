876172f237ad8ea8deb56629e26c2c762a5daadb
RenoBitkub <LemonBitKubCommunity@gmail.com>
2025-04-02 17:59:09 +0700

    (Setting).lua

--- Mobile/huyzCuTo/Loading_UI/Bloxfruits/(Setting).lua
+++ Mobile/huyzCuTo/Loading_UI/Bloxfruits/(Setting).lua
@@ -10,7 +10,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
         Title = "Key System",
         Description = "https://discord.gg/AfcRjyTd",
         KeyLink = "https://scwz.me/ww7hPv",
-        Keys = {"LEMON_LN6XL-WM1Z7-ZV4N-TJG2G-SMGOP"},
+        Keys = {"4DFJVWZDB5"},
         Notifi = {
         Notifications = true,
         CorrectKey = "Correct Key Loading Script...",
@@ -47,13 +47,201 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/RenoBitkub/LemonHub/r
   })
 
    AddButton(Tab2o, {
-     Name = "Lemon Hub Premium",
+     Name = "KaiTun Lemon Hub",
     Callback = function()
 	  local Settings = {
   JoinTeam = "Pirates"; -- Pirates/Marines
   Translator = true; -- true/false
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RenoBitkub/LemonV2/refs/heads/main/Mobile/Lemonreal/skibidi/Loading_GUI/(settings).lua"))()
+getgenv().ConfigsKaitun = {
+
+	["Safe Mode"] = false, -- Will be pass all anti cheat (but slow farm)
+
+	
+
+	["Melee"] = {
+
+		["Death Step"] = true,
+
+		["Electric Claw"] = true,
+
+		["Dragon Talon"] = true,
+
+		["Sharkman Karate"] = true,
+
+		["Superhuman"] = true,
+
+		["God Human"] = true,
+
+	},
+
+
+
+	["Sword"] = {
+
+		-- : World 1
+
+		["Saber"] = true,
+
+		["Pole"] = true,
+
+		-- : World 2
+
+		["Midnight Blade"] = false,
+
+		["Shisui"] = false,
+
+		["Saddi"] = false,
+
+		["Wando"] = false,
+
+		["Rengoku"] = true,
+
+		["True Triple Katana"] = false,
+
+		-- : World 3
+
+		["Yama"] = true,
+
+		["Tushita"] = true,
+
+		["Canvander"] = true,
+
+		["Buddy Sword"] = true,
+
+		["Twin Hooks"] = true,
+
+		["Hallow Scythe"] = true,
+
+		["Cursed Dual Katana"] = true,
+
+	},
+
+
+
+	["Gun"] = {
+
+		-- : World 2
+
+		["Kabucha"] = false,
+
+		-- : World 3
+
+		["Venom Bow"] = true,
+
+		["Skull Guitar"] = true,
+
+	},
+
+
+
+	["Mastery"] = {
+
+		["Melee"] = true,
+
+		["Sword"] = true,
+
+		["Devil Fruits"] = false,
+
+
+
+		["Configs"] = {
+
+			["Selected All Sword"] = true,
+
+			["Select Sword"] = {"Cursed Dual Katana"},
+
+		}
+
+	},
+
+
+
+	["Race"] = {
+
+		["v2"] = true,
+
+		["v3"] = true,
+
+		["Locked"] = {
+
+			["Mink"] = true,
+
+			["Human"] = true,
+
+			["Skypiea"] = true,
+
+			["Fishman"] = false,
+
+		},
+
+	},
+
+
+
+	["Fruit"] = {
+
+		["Main Fruit"] = {"Dragon-Dragon"},
+
+		["Sec Fruit"] = {"Dragon-Dragon"},
+
+		["Safe Fruit"] = {
+
+			"Dough-Dough",
+
+			"Dragon-Dragon"
+
+		},
+
+	},
+
+
+
+	["Quest"] = {
+
+		["Rainbow Haki"] = true,
+
+		["Pull Lever"] = true,
+
+		["Musketeer Hat"] = true,
+
+		["Dough Mirror"] = true,
+
+		["Shark Anchor"] = {
+
+			["Enable"] = false,
+
+			["Money"] = 25_000_000,
+
+		},
+
+	},
+
+
+
+	["Currency"] = {
+
+		["Lock Fragment"] = 25_000,
+
+	},
+
+
+
+	["Performance"] = {
+
+		["White Screen"] = false,
+
+		["Booster FPS"] = false,
+
+		["Lock FPS"] = 240,
+
+		["AFK Timeout"] = 150,
+
+	},
+
+}
+
+loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/73mkp0aqyfo4ypy8hvl0nz10lq49fey5.lua"))()
   end
   })
